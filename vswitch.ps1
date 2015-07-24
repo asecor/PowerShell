7a6f02b31ce6b689d929e77d89cb06775976a97c
asecor <commit@asecor.com>
2015-07-24 22:42:07 +0200

    First

---
+++ vswitch.ps1
@@ -0,0 +1,13 @@
+﻿$vmhost = Connect-VIServer 10.0.0.114 -User root -Password Password1
+$vswitch = Get-VirtualSwitch 
+if ($vswitch -eq "vSwitch0")
+{
+New-VirtualPortGroup -VirtualSwitch $vswitch -Name Servers -VLanId 22 -Server $vmhost
+New-VirtualPortGroup -VirtualSwitch $vswitch -Name Development -VLanId 122 -Server $vmhost
+New-VirtualPortGroup -VirtualSwitch $vswitch -Name Testing -VLanId 143 -Server $vmhost
+}
+else
+{
+Write-host "Finished"
+}
+
