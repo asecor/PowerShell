adcf5570a20cac172a59beeb22b79ca28416d8e7
asecor <asecor@users.noreply.github.com>
2020-06-29 09:14:54 +0200

    Added file size options

--- Make-NDF.ps1
+++ Make-NDF.ps1
@@ -2,6 +2,10 @@
 $sqlins = "localhost"
 # Specify how many NDFs should be created per mountpoint
 $ndfs = 5
+# Specify file properties such as SIZE, MAXSIZE & FILEGROWTH
+$ndfsize = "10MB"
+$ndfmaxsize = "50MB"
+$ndffilegrowth = "1MB"
 # Getting local mountpoints
 $mountpoints = Get-WmiObject -Class Win32_Volume | Where-Object{$_.Label -like "D*"} | Select-Object -ExpandProperty Name
 
@@ -23,7 +27,7 @@ Foreach ($mount in $mountpoints) {
         #This needs a bit of more work to make the DB name into a variable, for now, DB 7 is selected
         $database = Invoke-Sqlcmd -ServerInstance $sqlins 'select name from master.sys.databases where database_id = 7' | select name
         # Using T-SQL to alter the database by creating and adding the desired number of NDF files
-        $query = "ALTER DATABASE $($database.Name) ADD FILE ( NAME = $($database.Name)_$($mpname)_$($i), FILENAME = '$($mount)$servicename\$MSSQL\$Data\$($database.Name)_$($i).ndf' )"
+        $query = "ALTER DATABASE $($database.Name) ADD FILE ( NAME = $($database.Name)_$($mpname)_$($i), FILENAME = '$($mount)$servicename\$MSSQL\$Data\$($database.Name)_$($i).ndf', SIZE = $($ndfsize), MAXSIZE = $($ndfmaxsize), FILEGROWTH = $($ndffilegrowth))"
         # Executing the previous T-SQL query
         Invoke-Sqlcmd -ServerInstance $sqlins -Database $($database.name) -Query $query
     }
